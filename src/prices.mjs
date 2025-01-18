5779adf071bbd618ea579bbeaa424b47e1c8e91c
zsonbi <zsomborder@gmail.com>
2025-01-18 18:52:34 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -6,8 +6,7 @@ import express from "express";
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
 
 function convert(date){
-  if(date instanceof Date){
-  }
+
 
 }
 
