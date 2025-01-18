26b43ec989ab45a7877552c97c0ef8e612a2bb11
zsonbi <zsomborder@gmail.com>
2025-01-18 18:51:59 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -96,8 +96,6 @@ function createApp(database) {
       let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date && date.equals(holiday)
-        // date.getFullYear() === holiday.getFullYear() &&
-
       ) {
         return true;
       }
